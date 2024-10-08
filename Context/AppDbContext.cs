3fa4e3cdc694661970f9452aaca0c47ef1e65169
Ghanshyam_Patel <patelsarkar2255@gmail.com>
2024-10-08 17:10:44 +0530

    First Commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagement.Models;                                                                                   
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagement.Models
+{
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public int quantity { get; set; }
+
+        [Required]
+        public decimal price { get; set; }
+
+       
+        public bool IsActive { get; set; } = true;
+
+        [Required]
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? ModifiedAt { get; set; }
+
+        public string ModifiedBy { get; set; }
+    }
+}
